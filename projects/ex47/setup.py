c747efbae88596e9e4069a14cde96bc92f321daf
weston <wnealdevelopment@gmail.com>
2012-07-27 08:14:44 -0700

    new exercises are up

---
+++ projects/ex47/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description':'My Project',
+    'author': 'Weston Neal',
+    'url': 'Url to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'wnealdevelopment@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
